0fa20c192cee8819bfca2eaf13f41e9c0e02f3df
Prishat <prishatbachhar@gmail.com>
2023-02-08 19:37:03 +0530

    config file added

---
+++ config.py
@@ -0,0 +1,18 @@
+# Model Parameter Dictionaries
+rmtpp_params = {"model":"rmtpp",
+                "seq_len":10,
+                "emb_dim":10,
+                "hid_dim":32,
+                "mlp_dim":16,
+                "dropout":0.1,
+                "alpha":0.05,
+                "batch_size":1024,
+                "event_class":7,
+                "verbose_step":350,
+                "importance_weight":"store_true",
+                "lr":1e-3,
+                "epochs":30}
+
+# Datset Parameter Dictionaries
+
+atm_params = {'seq_len': 10}
